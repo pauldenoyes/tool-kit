da5db11e37c46f312c625e25b6d670958133be9f
pauldenoyes <43006315+pauldenoyes@users.noreply.github.com>
2018-12-04 10:17:22 +0100

    Update RNN-LSTM-NLP-Example.py

--- RNN-LSTM-NLP-Example.py
+++ RNN-LSTM-NLP-Example.py
@@ -16,12 +16,12 @@ numberOfLSTMLayers = 2
 
 #Raw data : Here in UTF-8
 reader = csv.reader(open(r"myDataFile.csv", encoding = 'utf-8'), delimiter = ";")
-myDatas = np.array(list(reader))
-labels = array(list(myDatas[:, 1]))
+mydata = np.array(list(reader))
+labels = array(list(mydata[:, 1]))
 
-#Pre-treatment of raw datas (example lowercase or remove punctuation)
+#Pre-treatment of raw data (example lowercase or remove punctuation)
 ##Lowercasing
-myDatas[:,0] = np.array(list(map(str.lower, myDatas[:,0])))
+mydata[:,0] = np.array(list(map(str.lower, mydata[:,0])))
 ##Replace accents and add space between word and punctuation so that "toto!" is not
 def strip_accents(text): #Remove accents and finaly punctuation too
     try:
@@ -34,15 +34,15 @@ def strip_accents(text): #Remove accents and finaly punctuation too
     table = str.maketrans({key: None for key in string.punctuation})
     text = text.translate(table)
     return str(text)
-for i in range (0, len(myDatas[:,0])):
-    myDatas[i,0] = strip_accents(myDatas[i,0])
+for i in range (0, len(mydata[:,0])):
+    mydata[i,0] = strip_accents(mydata[i,0])
     #Remove punctuation accolate to words
-    myDatas[i,0] = str(' '.join(re.findall(r"\w+|[^\w\s]", myDatas[i,0], re.UNICODE)))
+    mydata[i,0] = str(' '.join(re.findall(r"\w+|[^\w\s]", mydata[i,0], re.UNICODE)))
 
 #Keras tokenization
 from keras.preprocessing.text import one_hot
 from keras.preprocessing.sequence import pad_sequences
-docs = list(myDatas[:,0])
+docs = list(mydata[:,0])
 encoded_docs = [one_hot(word, vocab_size) for word in docs]
 
 #Padding or truncate to make each word sequence the same length
@@ -67,15 +67,15 @@ print(model.summary())
 #Compile model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
 
-#Evaluate on test datas
-reader = csv.reader(open(r"myNewDatas.csv", encoding = 'utf-8'), delimiter = ";")
-myDatas = np.array(list(reader))
-labels2 = array(list(myDatas[:, 1]))
-myDatas[:,0] = np.array(list(map(str.lower, myDatas[:,0])))
-for i in range (0, len(myDatas[:,0])):
-    myDatas[i,0] = strip_accents(myDatas[i,0])
-    myDatas[i,0] = str(' '.join(re.findall(r"\w+|[^\w\s]", myDatas[i,0], re.UNICODE)))
-docs = list(myDatas[:,0])
+#Evaluate on test data
+reader = csv.reader(open(r"myNewdata.csv", encoding = 'utf-8'), delimiter = ";")
+mydata = np.array(list(reader))
+labels2 = array(list(mydata[:, 1]))
+mydata[:,0] = np.array(list(map(str.lower, mydata[:,0])))
+for i in range (0, len(mydata[:,0])):
+    mydata[i,0] = strip_accents(mydata[i,0])
+    mydata[i,0] = str(' '.join(re.findall(r"\w+|[^\w\s]", mydata[i,0], re.UNICODE)))
+docs = list(mydata[:,0])
 encoded_docs = [one_hot(word, vocab_size) for word in docs]
 padded_docs2 = pad_sequences(encoded_docs, maxlen=maxAuthorizedSiseOfSentencesInWords, padding='post')
 
