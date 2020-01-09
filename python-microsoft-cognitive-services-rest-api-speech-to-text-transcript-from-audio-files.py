4adc701af976c65a77b5ce5706d8936fd2eed45f
pauldenoyes <43006315+pauldenoyes@users.noreply.github.com>
2020-01-09 22:04:42 +0100

    Create python-microsoft-cognitive-services-rest-api-speech-to-text-transcript-from-audio-files.py

---
+++ python-microsoft-cognitive-services-rest-api-speech-to-text-transcript-from-audio-files.py
@@ -0,0 +1,30 @@
+#Python code to call the speech-to-text REST API proposed by Microsoft Cognitive services. It will extract the text transcript of a
+#bunch of audio files. You need a subscription to this service (that can be free under certain conditions)
+#You also need to encode the files under the appropriate .wav format, for instance bit-rate 16000, Mono, 16 bit.
+
+import requests
+import os
+
+#List of file names to treat
+listOfFiles = os.listdir(r'C:\Users\user\myFiles')
+
+#Personal credentials and endpoints
+subscription_key = 'MySubscriptionKey'
+api_end_point = 'https://westus.api.cognitive.microsoft.com/sts/v1.0'
+
+#Get the bearer token
+fetch_token_url = api_end_point + '/issueToken'
+headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+r_bearer = requests.post(fetch_token_url, headers=headers)
+bearer_access_token = r_bearer.text
+#Call the speech-to-text Microsoft API with the bearer token to extract text from audio files
+api_end_point = 'https://westus.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=en-EN' #Here specify the appropriate cultural
+headers = {'Authorization': 'Bearer ' + bearer_access_token,
+           'Host': 'westus.stt.speech.microsoft.com',}
+Text_vs_AudioFileName = []
+for fileName in listOfFiles:
+    with open(r'C:\\Users\\user\\myFiles\\' + fileName, 'rb') as data:
+        r = requests.post(url = api_end_point, data = data, headers=headers).json()     #Send POST request and store the reply in 'r', converted in JSON format
+        extracted_text = r['DisplayText']   #Extract text from the JSON reply
+        Text_vs_AudioFileName.append([extracted_text, fileName])
+print(Text_vs_AudioFileName)
